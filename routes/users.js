2ac1f9d82526de8bbd619d19d3af295538bd768d
Chivan <huainian361@hotmail.com>
2019-05-04 20:45:48 +0800

    修改登处理逻辑

--- routes/users.js
+++ routes/users.js
@@ -9,40 +9,48 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function (req, res, next) {
-  console.log('searching...');
+    console.log(Date.now()+':user login');
   let params = req.query;
   let name = params['name'];
   let password = params['password'];
-  conn.query(userSql.login,[name,password],function (err, result) {
-    // console.log(err);
-    if(err){
-      console.log(err);
-      res.send("{result:'error', status:'201'}");
-    }else{
-      console.log(result[0]);
-      res.send("{result:"+JSON.stringify(result[0])+", status:'200'}");
+    let isEmpty =(name == null || password == null);
+    if(isEmpty){
+        res.send("{result:'error', status:'101'}");
+    }else {
+        conn.query(userSql.login, [name, password], function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send("{result:'error', status:'201'}");
+            } else {
+                console.log(result[0]);
+                res.send("{result:" + JSON.stringify(result[0]) + ", status:'200'}");
+            }
+        });
     }
-  });
 });
 
-router.put('/regist', function (req, res, next) {
-  console.log(Date.now()+':user login');
+router.post('/regist', function (req, res, next) {
+  console.log(Date.now()+':user regist');
   let params = req.body;
   let name = params['name'];
   let phone = params['phone'];
   let password = params['password'];
-  console.log(req.body);
+  console.log(req.body.data);
   console.log(req.params);
   console.log(req.query);
-  conn.query(userSql.regist,[name,password, phone],function (err, result) {
-    // console.log(err);
-    if(err){
-      console.log(err);
-      res.send("{result:'error', status:'201'}");
-    }else{
-      res.send("{result:'ok', status:'200'}");
-    }
-  });
+  console.log(name == null);
+  let isEmpty =(name == null || phone == null || password == null);
+  if(isEmpty){
+      res.send("{result:'error', status:'101'}");
+  }else{
+      conn.query(userSql.regist,[name,password, phone],function (err, result) {
+        if(err){
+          res.send("{result:'error', status:'102'}");
+        }else{
+          res.send("{result:'ok', status:'100'}");
+        }
+      });
+  }
 
 });
 
