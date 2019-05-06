c3aad1e696ba2356400b654518f3e00308dc2fe3
Chivan <huainian361@hotmail.com>
2019-05-07 01:45:21 +0800

    添加商铺接口

--- routes/shop.js
+++ routes/shop.js
@@ -17,27 +17,23 @@ router.get('/getAllShop', function(req, res, next) {
                 let shop = result[m];
                 // urls.push('select * from Barber where shop='+shop['id']);
                 urls+=('select * from Barber where shop='+shop['id'])
-                urls+=',';
-                console.log(urls);
+                urls+=';';
             }
             urls = urls.substring(0,urls.length-1);
             if(urls.length>0) {
                 conn.query(urls, [], function (err, barbers) {
-                    // console.log(barbers);
+
                     if (err) {
                         console.log(err);
                         res.jsonp({'result': 'error', 'status': errorCode.dbError});
                     } else {
-                        console.log(barbers.length);
                         for(let i=0;i<barbers.length;i++){
-                            // urls.push('select * from Barber where shop='+shop['id']);
                             let str= JSON.stringify(result[i]);
-                            // console.log(JSON.parse(data));
                             let s = typeof str =='string'? JSON.parse(str):str;
                             s.barber = barbers[i];
                            result[i]=s;
                         }
-                        console.log('barber....');
+
                         res.jsonp({'result': result, 'status': errorCode.loginSuccess});
                     }
                 });
