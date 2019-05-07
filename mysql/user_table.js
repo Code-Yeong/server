f3498a6e567b558e8874789b8dca1c3f5c1b2932
yeongguo <yeongguo@tencent.com>
2019-05-07 20:29:31 +0800

    修改删除地址的接口

--- mysql/user_table.js
+++ mysql/user_table.js
@@ -3,8 +3,8 @@ let user = {
     'login':'select * from User where phone =? and password=?',
     'isPhoneExist': 'select * from User where phone=?',
     'updateLastLogin': 'update User set last_login=? where _id=?',
-    'searchAddress':'select * from Adderss where cusId=? and status!=0 order by last_modify DESC',
     'addAddress':'insert into Adderss(cusId,name,phone,address,status,midifyTime) values(?,?,?,?,?,?)',
+    'getAllAddress':'select * from Adderss where cusId=? and status!=0 order by last_modify DESC',
     'updateAddress':'update Adderss set name=?,phone=?,address=?,status=?,midifyTime=? where id=?',
     'deleteAddress':'update Adderss set status=0,midifyTime=? where id=?',
 };
