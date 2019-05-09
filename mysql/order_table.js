8b9d09d49f2b5404b59ed76b5a90c8d27cf7c65d
Chivan <huainian361@hotmail.com>
2019-05-09 22:52:59 +0800

    添加订单表

--- mysql/order_table.js
+++ mysql/order_table.js
@@ -4,7 +4,7 @@ let orderSql = {
     "updateOrderStatus":"update Reservation set status =2 where id=?",
     "getBarberOrder":"select * from Reservation where barberId=? order by createTime DESC",
     "getShopOrder":"select * from Reservation where shopId=? order by createTime DESC",
-    "getCusOrder":"select r.id,r.createTime,r.serveTime,r.finshedTime,r.commentTime,r.cusId,r.barberId,r.shopId,r.serveName,r.status,r.money,r.comment,b.name as staffName from Reservation r,Barber b where r.barberId=b.id and r.cusId=? order by createTime DESC",
+    "getCusOrder":"select r.id,r.createTime,r.serveTime,r.finshedTime,r.commentTime,r.cusId,r.barberId,r.shopId,r.serveName,r.status,r.money,r.comment,b.name as staffName,b.avatar as avatar from Reservation r,Barber b where r.barberId=b.id and r.cusId=? order by createTime DESC",
     "getBarberUnStartOrder":"select * from Reservation where barberId=? and status<3",
 };
 
