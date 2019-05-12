a5a9d6cd9d0bc1f0232142b49b9929fd24f6eb7e
Chivan <huainian361@hotmail.com>
2019-05-12 14:56:47 +0800

    添加订单表

--- mysql/order_table.js
+++ mysql/order_table.js
@@ -3,7 +3,7 @@ let orderSql = {
     "commentOrder":"update Reservation set status=4, comment=?,shopScpre=?,barberScore=?,commentTime=? where id=?",
     "getBarberOrder":"select * from Reservation where barberId=? order by createTime DESC",
     "getShopOrder":"select * from Reservation where shopId=? order by createTime DESC",
-    "getCusOrder":"select r.id,r.createTime,r.address,r.serveTime,r.finshedTime,r.commentTime,r.cusId,r.barberId,r.shopId,r.serveName,r.status,r.money,r.comment,b.name as staffName,b.avatar as avatar from Reservation r,Barber b where r.barberId=b.id and r.cusId=? order by createTime DESC",
+    "getCusOrder":"select r.id,r.createTime,r.address,r.serveTime,r.finshedTime,r.commentTime,r.cusId,r.barberId,r.shopId,r.serveName,r.status,r.money,r.comment,b.name as staffName,b.avatar as avatar,r.code as code from Reservation r,Barber b where r.barberId=b.id and r.cusId=? order by createTime DESC",
     "getStaffOrder":"select r.id,r.createTime,r.address,r.serveTime,r.finshedTime,r.commentTime,r.cusId,r.barberId,r.shopId,r.serveName,r.status,r.money,r.comment,u.name as cusName,u.avatar as avatar from Reservation r,User u where r.cusId=u._id and r.barberId=? order by serveTime ASC",
     "getBarberUnStartOrder":"select * from Reservation where barberId=? and status<3",
     "updateOrderStatus":"update Reservation set status=? where id=?"
