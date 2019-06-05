f8aa4221b405d66d93862cbe2259ca0f9c4f57e7
Chivan <huainian361@hotmail.com>
2019-06-05 21:23:58 +0800

    理发师申请加入店铺

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let cors = require('cors');
 
 let indexRouter = require('./routes/index');
 let uploadRouter = require('./routes/upload');
@@ -20,6 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -40,11 +42,11 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-  res.setHeader("Access-Control-Max-Age", "3600");
-  res.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+  // res.setHeader("Access-Control-Max-Age", "3600");
+  // res.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
+  // res.setHeader("Access-Control-Allow-Credentials", "true");
 
   // set locals, only providing error in development
   res.locals.message = err.message;
