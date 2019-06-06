b4e5299a54f4b9f938b7419020eb627abb66cef7
Chivan <huainian361@hotmail.com>
2019-06-06 13:19:56 +0800

    新增管理员添加店铺接口

--- mysql/shop_table.js
+++ mysql/shop_table.js
@@ -1,7 +1,7 @@
 let shop = {
     'getAllShops':'select * from Shop',
     'getShopStatistic':'select avg(shopScpre) as score, count(shopId) as orderCount from Reservation where shopId=?',
-    'addShop':'INSERT INTO `Shop(id,name,ownner,avatar,level,status,address,license,auth_method,description,regist_time,last_modify)` VALUES (?,?,?,?,?,?,?,?,?,?,?,?)',
+    'addShop':'INSERT INTO Shop(id,name,ownner,avatar,level,status,address,license,auth_method,description,regist_time,last_modify) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)',
 };
 
 module.exports = shop;
