6121d3144f369d0e008a730866ab87171aabbdb6
Chivan <huainian361@hotmail.com>
2019-06-09 18:30:33 +0800

    管理员查询订单接口

--- mysql/admin_table.js
+++ mysql/admin_table.js
@@ -1,6 +1,7 @@
 let adminSql = {
     'login':'select * from Admin where phone=? and password=?',
     'queryAllOrder':'select * from Reservation order by createTime desc',
+    "getStaffOrder":"select r.id,r.createTime,r.address,r.serveTime,r.finshedTime,r.commentTime,r.cusId,r.barberId,r.shopId,r.serveName,r.status,r.money,r.comment,u.name as cusName,u.avatar as avatar,r.verified as verified,r.code as code from Reservation r,User u where r.cusId=u._id order by serveTime ASC",
 };
 
 module.exports = adminSql;
